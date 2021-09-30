8429ff50a7ecfd9c348118ed280c36b9a5778171
metalsadman <aldringm321@gmail.com>
2021-09-30 09:15:07 +0800

    1 file modified

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    TEST
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
     <q-btn label="Test Dialog" @click="testDialog" />
   </q-page>
 </template>
